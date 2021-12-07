625e9e520f8e7276330ef030fac73a43d044dcac
Iztok Bajcar <ib8548@student.uni-lj.si>
2021-12-07 09:45:30 +0100

    Added GNOME desktop, fixed ubridge

--- provision.sh
+++ provision.sh
@@ -1,2 +1,14 @@
-sudo add-apt-repository ppa:gns3/ppa
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y gns3-server gns3-gui 
+
+export DEBIAN_FRONTEND=noninteractive 
+
+# Adds GNS3 ppa repository (and updates the package lists)
+add-apt-repository ppa:gns3/ppa
+
+# Desktop
+apt-get install -y gnome-session gnome-terminal
+
+# Installs GNS3
+apt-get install -y gns3-server gns3-gui
+
+usermod -aG ubridge vagrant
+reboot
